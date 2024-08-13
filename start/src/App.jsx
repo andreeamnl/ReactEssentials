f8e891436a08ed5733b52db377936b44f601f5c3
OzzyOsbourne <andreeamanole2020@gmail.com>
2024-08-13 15:37:23 +0300

    IMPROVED:Rendering content conditionally.

--- start/src/App.jsx
+++ start/src/App.jsx
@@ -13,7 +13,19 @@ function App() {
 
   function handleSelect(selectedButton){
     setSelectedTopic(selectedButton);
-    //console.log(selectedButton);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+  if (selectedTopic){
+    tabContent = (<div id="tab-content">
+    <h3>
+      {EXAMPLES[selectedTopic].title}</h3>
+    <p>
+      {EXAMPLES[selectedTopic].description}</p>
+    <pre>
+      <code>{EXAMPLES[selectedTopic].code}</code></pre>
+
+  </div>);
   }
 
   return (
@@ -54,16 +66,7 @@ function App() {
           <TabButton onSelect = {() =>handleSelect('state')} >State</TabButton>
 
           </menu>
-          {!selectedTopic ? <p>Please select a topic.</p> : null}
-          {selectedTopic ? (<div id="tab-content">
-            <h3>
-              {EXAMPLES[selectedTopic].title}</h3>
-            <p>
-              {EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code></pre>
-
-          </div>) : null }
+          {tabContent}
 
         </section>
       </main>
